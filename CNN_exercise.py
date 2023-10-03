02d1626415755bbe1996ce80f141cf6c94aaacb4
seoljiwon <suljiwon@naver.com>
2023-10-03 20:26:25 +0900

    feat: lecture 9 CNN

---
+++ CNN_exercise.py
@@ -0,0 +1,89 @@
+import torch
+import torch.nn.functional as F
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+# Training settings
+batch_size = 64
+
+# MNIST Dataset
+train_dataset = datasets.MNIST(
+    root="data", train=True, transform=transforms.ToTensor(), download=True
+)
+test_dataset = datasets.MNIST(root="data", train=False, transform=transforms.ToTensor())
+
+
+# Data Loader (Input Pipeline)
+train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=False)
+test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+
+
+class Net(torch.nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = torch.nn.Conv2d(1, 16, kernel_size=3)
+        self.conv2 = torch.nn.Conv2d(16, 32, kernel_size=3)
+        self.conv3 = torch.nn.Conv2d(32, 16, kernel_size=3)
+        self.mp = torch.nn.MaxPool2d(2)
+        self.fc1 = torch.nn.Linear(16, 512)
+        self.fc2 = torch.nn.Linear(512, 512)
+        self.fc3 = torch.nn.Linear(512, 10)
+
+    def forward(self, x):
+        in_size = x.size(0)
+        x = F.relu(self.mp(self.conv1(x)))
+        x = F.relu(self.mp(self.conv2(x)))
+        x = F.relu(self.mp(self.conv3(x)))
+        x = x.view(in_size, -1)  # flatten the tensor
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        return self.fc3(x)
+
+
+model = Net()
+
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+
+
+def train(epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = Variable(data), Variable(target)
+
+        output = model(data)
+        loss = criterion(output, target)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch_idx % 100 == 0:
+            print(
+                f"Train Epoch: {epoch} [{batch_idx*len(data)}/{len(train_loader.dataset)} ({100.0*batch_idx/len(train_loader):.0f}%)]\tLoss: {loss.item():.6f}"
+            )
+
+
+def test():
+    model.eval()
+    test_loss = 0
+    correct = 0
+    for data, target in test_loader:
+        data, target = Variable(data, volatile=True), Variable(target)
+
+        output = model(data)
+        test_loss += criterion(output, target).item()
+
+        pred = torch.max(output.data, 1)[1]
+        correct += pred.eq(target.data.view_as(pred)).sum()
+
+    test_loss /= len(test_loader.dataset)
+    print(
+        f"\nTest set: Average loss: {test_loss:.4f}, Accuracy: {correct}/{len(test_loader.dataset)} ({100.0*correct/len(test_loader.dataset):.0f}%)\n"
+    )
+
+
+for epoch in range(1, 10):
+    train(epoch)
+    test()
